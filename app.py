f552699b9a223d6a5e72404829d6301bc4c4ca20
Terry Chiem <terrychiem@gmail.com>
2020-03-27 16:03:46 -0700

    Completed part one

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 """Flask app for adopt app."""
 
-from flask import Flask, render_template, redirect
-
+from flask import Flask, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
-
 from models import db, connect_db, Pet
 from seed import setup_db
-
+from forms import AddPetForm, EditPetForm
 
 app = Flask(__name__)
 
@@ -17,7 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-# db.create_all()
+db.create_all()
 
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
@@ -27,11 +25,57 @@ connect_db(app)
 toolbar = DebugToolbarExtension(app)
 
 # run seed file with sample data
-setup_db()
+# setup_db()
 
 @app.route('/')
 def list_pets():
+    """ Show a list of all pets """
 
     pets = Pet.query.all()
 
     return render_template('index.html', pets=pets)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pet():
+    """ Form for adding a pet """
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data or None
+        age = form.age.data
+        notes = form.notes.data
+        flash(f"Added {name} to adoption list.")
+
+        # create a pet instance
+        new_pet = Pet(name=name, species=species, photo_url=photo_url, age=age, notes=notes)
+
+        db.session.add(new_pet)
+        db.session.commit()
+
+        return redirect('/')
+    
+    else:
+        return render_template('add_pet_form.html', form=form)
+
+@app.route('/<int:pet_id>', methods=['GET', 'POST'])
+def show_pet_details(pet_id):
+    """ Show a pet's details """
+
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        pet.photo_url = form.photo_url.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        
+        db.session.commit()
+        flash(f"Pet {pet.name} updated!")
+        return redirect('/')
+
+    else:
+        return render_template('pet_details.html', pet=pet, form=form)
--- forms.py
+++ forms.py
@@ -1,1 +1,35 @@
 """Forms for adopt app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField
+from wtforms.validators import InputRequired, Optional, URL, AnyOf, Length
+
+
+class AddPetForm(FlaskForm):
+    """ Form for adding pets """
+
+    name = StringField("Pet Name",
+                        validators=[InputRequired()])
+
+    species = StringField("Species",
+                        validators=[InputRequired(""),
+                        AnyOf(["cat", "dog", "porcupine"])])
+
+    photo_url = StringField("Photo URL",
+                        validators=[Optional(), URL()])
+
+    age = StringField("Age",
+                        validators=[InputRequired(),
+                                    Length(min=0, max=30)])
+
+    notes = StringField("Notes")
+
+
+class EditPetForm(FlaskForm):
+    """ Form for editing a pet """
+
+    photo_url = StringField("Photo URL",
+                        validators=[Optional(), URL()])
+    
+    notes = StringField("Notes")
+
+    available = BooleanField("Available")
